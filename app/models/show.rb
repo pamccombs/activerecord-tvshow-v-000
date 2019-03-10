8c12e8b91eb6e7dad2f21367a26aa7d1dc40dcf4
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-10 13:15:49 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -11,5 +11,9 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
       self.minimum(:rating)
   end
+  
+  def self.least_popular_show
+      self.where("rating = ?", self.lowest_rating).first
+  end
     
 end
