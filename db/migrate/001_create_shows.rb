d3fde1d138432c20c939cacbe2690d125509fb0b
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-10 13:10:34 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,10 @@
 class CreateShows < ActiveRecord::Migration
   def change
-    create_table :shows {|show|
+    create_table :shows do |show|
       show.string :name
       show.string :day
       show.string :network
       show.integer :rating
-    }
+    end
   end
 end
